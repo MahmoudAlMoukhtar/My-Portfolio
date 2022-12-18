788276c4a7559fb0e37b24f4ce10738326f69a7a
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2022-12-18 10:18:20 +0200

    update design

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -30,16 +30,22 @@ const ItemProjectsDashpoard = ({project, removeProject}) => {
 const DashboardPage = ({projects, addProjectAction, removeProject}) => {
   return (
     <div className="flex flex-col gap-16 w-full items-center bg-white text-black z-40 p-8">
-      <nav className="flex justify-between bg-slate-800 text-white w-full gap-20 p-2">
+      <nav className="flex justify-start bg-slate-800 text-white w-full gap-2 p-2">
         <button
           onClick={() => {
             //console.log("logout");
             firebase.auth().signOut();
           }}
+          className="bg-white p-1 text-black font-semibold"
         >
           LOGOUT
         </button>
-        <Link to={"/dashboard/add"}>Add Project</Link>
+        <Link
+          to={"/dashboard/add"}
+          className="bg-white p-1 text-black font-semibold"
+        >
+          Add Project
+        </Link>
       </nav>
       <div className="w-[100%] flex flex-col justify-center gap-6">
         <h1 className="text-center">Projects:</h1>
